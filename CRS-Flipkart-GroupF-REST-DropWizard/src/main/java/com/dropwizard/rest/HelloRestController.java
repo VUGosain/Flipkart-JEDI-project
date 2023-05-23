b584f0f99864c41a4b16c68fd855a7e2415c27e4
avantika <avantika.mudumbai@gmail.com>
2023-05-23 22:17:32 +0530

    drop wizard server added

---
+++ CRS-Flipkart-GroupF-REST-DropWizard/src/main/java/com/dropwizard/rest/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.dropwizard.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloRestController {
+    @GET
+    public String getEmployees(){
+        return "it worked";
+    }
+}
